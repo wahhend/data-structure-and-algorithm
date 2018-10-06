43bf123fdb275cabf34766ac2c6a78ee725043b7
wahhend <whendrohartono@gmail.com>
2018-10-06 15:40:43 +0700

    minor repair

--- LinkedList.java
+++ LinkedList.java
@@ -11,58 +11,80 @@ class LinkedList{
     Node head;
     Node tail;
 
+    public boolean isEmpty(){
+        return head==null? true:false;
+    }
+
+    //add data as first node in list
     public void addFirst(int data){
         Node newNode = new Node(data);
-
-        if(head==null){
+        //case for empty list
+        if(isEmpty()){
             head = newNode;
-            tail = newNode;
+            tail = head;
         }
+        //case for adding node in the start of current list
         else{
             newNode.next = head;
             head = newNode;
         }
     }
 
+    //add data as last node in list
     public void addLast(int data){
         Node newNode = new Node(data);
-
-        if(head==null){
-            head = newNode;
+        //case for empty list
+        if(isEmpty()){
             tail = newNode;
+            head = tail;
         }
+        //case for adding node in the end of current list
         else{
             tail.next = newNode;
             tail = newNode;
         }
+        
     }
 
+    //add node after target value
     public void addAfter(int target, int data){
         Node newNode = new Node(data);
         Node current = head;
-
-        while(current.data!=target||current!=null){
+        //insert node in the middle of the list
+        while(current!=tail){
+            if(current.data == target){
+                newNode.next = current.next;
+                current.next = newNode;
+                return;
+            }
             current = current.next;
         }
-        if(current.data==target){
-            newNode.next = current.next;
-            current.next = newNode;
+        //special case if the target is tail
+        if(current.data == target){
+            addLast(data);
         }
     }
 
+    //remove first node
     public int removeHead(){
-        int delete = head.data;
+        Node deleted = head;
         head = head.next;
-        return delete;
+        return deleted.data;
     }
 
-    public void remove(int target){
+    //remove last node
+    public int removeTail(){
+        Node deleted = tail;
         Node current = head;
-        while(current.next.data!=target){
+        //find node before tail
+        while(current.next != tail){
             current = current.next;
         }
 
-        current.next = current.next.next;
+        //delete last node, redirect tail pointer
+        current.next = null;
+        tail = current;
+        return deleted.data;
     }
 
     public void print(){
@@ -75,3 +97,18 @@ class LinkedList{
     }
 }
 
+class Main{
+    public static void main(String[] args){
+        LinkedList ll = new LinkedList();
+
+        ll.addFirst(10);
+        ll.addLast(5);
+        ll.addFirst(20);
+        ll.addAfter(20, 15);
+        ll.addAfter(5, 0);
+        ll.print();
+        ll.removeHead();
+        ll.removeTail();
+        ll.print();
+    }
+}
