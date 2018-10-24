76133ff0e1e07219b854989f542a19fe60840263
wahhend <whendrohartono@gmail.com>
2018-10-24 08:28:53 +0700

    improvement ArrayStack

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,20 +1,38 @@
-class ArrayStack{
+class ArrayStackQueue{
     int[] arr = new int[0];
 
     public void addFirst(int data){
         int[] arr2 = new int[arr.length+1];
+
         arr2[0] = data;
-        for(int i=1;i<arr2.length;i++)
-            arr2[i] = arr[i-1];
+
+        for(int i=0;i<arr.length;i++){
+            arr2[i+1] = arr[i];
+        }
+
         arr = arr2;
     }
 
     public int removeFirst(){
         int[] arr2 = new int[arr.length-1];
-        for(int i=1;i<arr.length;i++)
-            arr2[i-1] = arr[i];
-        int pop = arr[0];
+        deleted = arr[0]
+
+        for(int i=0;i<arr2.length;i++){
+            arr2[i] = arr[i+1];
+        }
+
         arr = arr2;
-        return pop;
+
+        return deleted;
+    }
+
+    public void addLast(int data){
+        int[] arr2 = new int[arr.length+1];
+
+        for(int i=0;i<arr.length;i++){
+            arr2[i] = arr[i]
+        }
+
+        arr2[arr2.length] = data;
     }
 }
